a500c37b39a8b0ea18be3b5a3892abd04a3032bf
Dennis <denniszomar@gmail.com>
2024-08-07 19:49:18 +0000

    Keeping figure visibilty on in case of failed tests

--- test_plotLocations.m
+++ test_plotLocations.m
@@ -1,5 +1,7 @@
-% Setting figure visibility to off to speed up testing routine
-set(0, 'DefaultFigureVisible', 'off')
+% % Setting figure visibility to off to speed up testing routine
+% set(0, 'DefaultFigureVisible', 'off')
+% CAN'T DO THIS OR ELSE FAILED TESTS CAUSE FIGURE VISIBILITY TO NOT BE
+% RESTORED. NEED TO ADD PROPER SETUP AND TEARDOWN FUNCTIONALITY
 
 %% Test single input
 success = plotLocations("Location_AMDR.xlsx");
@@ -12,6 +14,7 @@ assert(success)
 %% Test three inputs
 success = plotLocations("Locations.xlsx","Type","TimeSpent");
 assert(success)
-
-% Setting figure visibility back on after testing
-set(0, 'DefaultFigureVisible', 'on')
+% 
+% 
+% % Setting figure visibility back on after testing
+% set(0, 'DefaultFigureVisible', 'on')
